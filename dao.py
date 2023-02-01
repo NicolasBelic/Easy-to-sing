dc0aa4e6b64cc090a0f32d57022b029535671eb5
NicolasBelic <123787064+NicolasBelic@users.noreply.github.com>
2023-01-31 21:34:28 -0300

    Update dao.py

--- dao.py
+++ dao.py
@@ -1,39 +1,65 @@
 
 import mysql.connector as banco
+import bcrypt
+import requests
+ 
+class usuario: 
+    def realiza_login(email,senha):
+        sql_select="select email from usuario where email =%s and senha=%s"
 
-def realiza_login(email,senha):
-    sql_select="select email from usuario where email =%s and senha=%s"
+        conexao=banco.connect(host="localhost", user="root", password="admin", database="easy_to_sing")
 
-    conexao=banco.connect(host="localhost", user="root", password="admin", database="easy_to_sing")
+        valores=(email,senha)
+        manipulador=conexao.cursor()
+        manipulador.execute(sql_select,valores)
+        return manipulador.fetchall()
 
-    valores=(email,senha)
-    manipulador=conexao.cursor()
-    manipulador.execute(sql_select,valores)
-    return manipulador.fetchall()
+    def insere_login_artista(nome,sexo,telefone,cpf,cep,email,senha):
+        sql_insert="insert into usuario (nome,sexo,telefone,cpf,cep,email,senha,tipo) values(%s,%s,%s,%s,%s,%s,%s,'artista')"
+        cripto = Criptografia()
+        senha_hash = cripto.criptografia(senha)
+        valores=(nome,sexo,telefone,cpf,cep,email,senha_hash)
 
-def insere_login_artista(nome,sexo,telefone,cpf,cep,email,senha):
-    sql_insert="insert into usuario (nome,sexo,telefone,cpf,cep,email,senha,tipo) values(%s,%s,%s,%s,%s,%s,%s,'artista')"
 
-    conexao=banco.connect(host="localhost", user="root", password="admin", database="easy_to_sing")
+        conexao=banco.connect(host="localhost", user="root", password="admin", database="easy_to_sing")
 
-    valores=(nome,sexo,telefone,cpf,cep,email,senha)
-    cursor=conexao.cursor()
-    cursor.execute(sql_insert,valores)
-    conexao.commit()
+        cursor=conexao.cursor()
+        cursor.execute(sql_insert,valores)
+        conexao.commit()
 
-def insere_login_restaurante(nome,telefone,cnpj,cep,email,senha):
-    sql_insert="insert into usuario (nome,telefone,cnpj,cep,email,senha,tipo) values(%s,%s,%s,%s,%s,%s,'restaurante')"
 
-    conexao=banco.connect(host="localhost", user="root", password="admin", database="easy_to_sing")
+    def insere_login_restaurante(nome,telefone,cnpj,cep,email,senha):
+        sql_insert="insert into usuario (nome,telefone,cnpj,cep,email,senha,tipo) values(%s,%s,%s,%s,%s,%s,'restaurante')"
+        conexao=banco.connect(host="localhost", user="root", password="admin", database="easy_to_sing")
+        cripto = Criptografia()
+        senha_hash = cripto.criptografia(senha)
+        valores=(nome,telefone,cnpj,cep,email,senha_hash)
+        cursor=conexao.cursor()
+        cursor.execute(sql_insert,valores)
+        conexao.commit()
 
-    valores=(nome,telefone,cnpj,cep,email,senha)
-    cursor=conexao.cursor()
-    cursor.execute(sql_insert,valores)
-    conexao.commit()
-    
-   class Criptografia:
+
+class Criptografia:
     def criptografia(self, texto):
             salt = bcrypt.gensalt()
             hash_valor = bcrypt.hashpw(texto.encode('utf-8'),salt)
             return hash_valor
+
+class Upload:
+    def fotos():
+        conexao=banco.connect(host="localhost", user="root", password="admin", database="easy_to_sing")
+        with open("image.jpg","rb") as f:
+           binary_data = f.read() 
+
+        cursor=conexao.cursor()
+        sql_insert="insert into upload (path,data_upload) values(%s,%s)"
+        cursor.execute(sql_insert,("nome_da_imagem",binary_data))
+
+        cursor.close()
+        conexao.close()
+
+    
+
+
+
 
